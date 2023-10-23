0209881cfb776458ca233a37075608d6febe305d
Yijun <github@yps.staut.land>
2023-10-23 13:46:53 +0200

    Update test

--- solution.test.js
+++ solution.test.js
@@ -3,45 +3,44 @@ const solution = require("./solution");
 describe("Testing the function solution", () => {
   const testCases = [
     // Test case 1
-    { input: "Ada was a pioneer in programming and algorithms.", expected: 4 },
+    {
+      input: "Chatgpt is a pioneer through programming.",
+      expected: 0 + 1 + 0 + 0 + 0,
+    },
     // Test case 2
     {
-      input:
-        "Java is a popular programming language in the area of web development.",
-      expected: 4,
+      input: "Dogs frolic with a stream.",
+      expected: 1 + 1 + 0 + 0 + 0,
     },
     // Test case 3
     {
-      input: "Data structures such as arrays and linked-lists are essential.",
-      expected: 5,
+      input: "Dogs frolic with a fountain.",
+      expected: 1 + 1 + 1 + 0 + 0,
     },
     // Test case 4
     {
-      input:
-        "Lambda calculus provides a theoretical foundation for functional programming.",
-      expected: 2,
+      input: "Dogs frolic with a fountain under the bench.",
+      expected: 1 + 1 + 1 + 1 + 0,
     },
     // Test case 5
     {
-      input:
-        "Sasha often wondered about the potential of artificial intelligence in the area of medical research.",
-      expected: 5,
+      input: "Dogs play around by the deep ocean",
+      expected: 1 + 1 + 1 + 1 + 1,
     },
     // Test case 6
     {
-      input: "In the arena of software design, a proper architecture is vital.",
-      expected: 4,
+      input: "From the first to the last letter, there lies a hint.",
+      expected: 0 + 1 + 0 + 0 + 1,
     },
-    // Test case 7
+    //Test case 7
     {
-      input: "Managing relational systems can be quite intricate.",
-      expected: 0,
+      input: "Programming constructs such as arrays are essential.",
+      expected: 0 + 3 + 0 + 0 + 0,
     },
     // Test case 8
     {
-      input:
-        "The coding gala was a celebration of algorithms and programming artistry.",
-      expected: 5,
+      input: "React is a popular programming language in web development.",
+      expected: 1 + 1 + 1 + 1 + 0,
     },
   ];
 
